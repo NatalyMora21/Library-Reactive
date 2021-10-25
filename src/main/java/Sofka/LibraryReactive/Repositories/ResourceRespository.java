a9fbb4e07aa65828ccc96a998b3c724c33388ff3
NatalyMora21 <tatamora21gmail.com>
2021-10-24 21:19:54 -0500

    Created reactive repository interface

---
+++ src/main/java/Sofka/LibraryReactive/Repositories/ResourceRespository.java
@@ -0,0 +1,8 @@
+package Sofka.LibraryReactive.Repositories;
+
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+
+public interface ResourceRespository extends ReactiveCrudRepository {
+
+
+}
